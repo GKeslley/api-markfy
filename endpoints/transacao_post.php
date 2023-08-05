71f946c37d8f950fbfe93799e41c2b523f05de00
guilherme <guilhermekeslley123@gmail.com>
2023-08-05 20:41:33 -0300

    payment endpoint

--- endpoints/transacao_post.php
+++ endpoints/transacao_post.php
@@ -2,42 +2,44 @@
   function api_transacao_post($request) {
   $user = wp_get_current_user();
   $user_id = $user->ID;
+  
+  $sell_product = $request['produto']['vendido'] === 'false';
 
   if ($user_id > 0) {
-    $nome = sanitize_text_field($request['nome']);
-    $preco = sanitize_text_field($request['preco']);
-    $descricao = sanitize_text_field($request['descricao']);
+    $product = json_encode($request['produto'], JSON_UNESCAPED_UNICODE);
+    $product_slug = sanitize_text_field($request['produto']['id']);
+    $product_name = sanitize_text_field($request['produto']['nome']);
+    $buyer_id = sanitize_text_field($request['comprador_id']);
+    $seller_id = sanitize_text_field($request['vendedor_id']);
+    $address = json_encode($request['endereco'], JSON_UNESCAPED_UNICODE);
 
-    $usuario_id = $user->user_login;
+    $buyer_email = get_userdata($user_id)->user_email;
+    $buyer_key = get_user_meta($user_id, 'unique_key', true);
+
+    $product_id = getproduct_id_by_slug($product_slug);
+    $sell_product = get_post_meta($product_id, 'vendido', true);
+    if ($sell_product === 'true') {
+      $error = new WP_Error('não encontrado', 'Produto não encontrado', array('status' => 404));
+      return rest_ensure_response($response);
+
+    }
+    update_post_meta($product_id, 'vendido', 'true');
 
     $response = array(
       'post_author' => $user_id,
-      'post_type' => 'produto',
-      'post_title' => $nome,
+      'post_type' => 'transacao',
+      'post_title' => $buyer_id . ' - ' . $product_name,
       'post_status' => 'publish',
       'meta_input' => array(
-        'nome' => $nome,
-        'preco' => $preco,
-        'descricao' => $descricao,
-        'usuario_id' => $usuario_id,
+        'comprador_id' => $buyer_id,
+        'vendedor_id' => $seller_id,
+        'endereco' => $address,
+        'produto' => $product,
         'vendido' => 'false'
       )
     );
 
-    $produto_id = wp_insert_post($response);
-    $response['id'] = get_post_field('post_name', $produto_id); 
-
-    $files = $request->get_file_params();
-
-    if ($files) {
-      require_once(ABSPATH . 'wp-admin/includes/image.php');
-      require_once(ABSPATH . 'wp-admin/includes/file.php');
-      require_once(ABSPATH . 'wp-admin/includes/media.php');
-
-      foreach ($files as $file => $array) {
-        media_handle_upload($file, $produto_id);
-      }
-    }
+    $post_id = wp_insert_post($response);
 
   } else {
     $response = new WP_Error('permissao', 'Usuário não possui permissão', array('status' => 401));
@@ -47,7 +49,7 @@
   }
 
   function registrar_api_transacao_post() {
-    register_rest_route('api', '/transacao', array(
+    register_rest_route('api', 'transacao', array(
       array(
         'methods' => WP_REST_Server::CREATABLE,
         'callback' => 'api_transacao_post',
